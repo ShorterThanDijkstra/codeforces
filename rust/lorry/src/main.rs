465146fa7f59398bd36a47f5ad5b840199733730
ShorterThanDijkstra <db1345569965@gmail.com>
2023-09-25 17:47:32 +0800

    lorry

--- rust/lorry/src/main.rs
+++ rust/lorry/src/main.rs
@@ -1,2 +1,17 @@
+use std::io::{stdin, BufRead, BufReader};
+
 fn main() {
+    let mut reader = BufReader::new(stdin());
+    let mut input = String::new();
+
+    reader.read_line(&mut input).unwrap();
+    let mut spilt = input.split_whitespace();
+    let count = spilt.next().unwrap().parse::<usize>().unwrap();
+    let cap = spilt.next().unwrap().parse::<usize>().unwrap();
+    let mut items = Vec::new();
+    for _ in 0..count {
+        input.clear();
+         reader.read_line(&mut input);
+         let space ;
+    }
 }
