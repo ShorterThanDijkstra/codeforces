3da48d8ac061de1e1d9e3cd19b1a86e9df7064fb
ShorterThanDijkstra <db1345569965@gmail.com>
2023-09-27 16:32:12 +0800

    lorry

--- rust/lorry/src/main.rs
+++ rust/lorry/src/main.rs
@@ -53,44 +53,77 @@ fn search_greedy(
                     if let Some((pre_choice1, pre_value1)) = pre_vehi1 {
                         if value2 > pre_value1 {
                             choices[pre_choice1] = index2;
-                            max = max - pre_value1 as u128 + value2 as u128 ;
+                            max = max - pre_value1 as u128 + value2 as u128;
                             cap -= 1;
                         }
                     }
                 } else {
-                    max += value2  as u128 ;
+                    max += value2 as u128;
                     cap -= 2;
                     choices.push(index2);
                 }
             } else {
                 pre_vehi1 = Some((choices.len(), value1));
                 vehicles1.pop();
-                max += value1  as u128 ;
+                max += value1 as u128;
                 cap -= 1;
                 choices.push(index1);
             }
-        } else if vehicles1.is_empty() {
+        } else if vehicles1.is_empty() && !vehicles2.is_empty() {
             let (value2, index2) = vehicles2.pop().unwrap();
             if cap == 1 {
                 if let Some((pre_choice1, pre_value1)) = pre_vehi1 {
                     if value2 > pre_value1 {
                         choices[pre_choice1] = index2;
-                        max = max - pre_value1  as u128  + value2 as u128 ;
+                        max = max - pre_value1 as u128 + value2 as u128;
                         cap -= 1;
                     }
                 }
             } else {
-                max += value2 as u128 ;
+                max += value2 as u128;
                 cap -= 2;
                 choices.push(index2);
             }
-        } else if vehicles2.is_empty() {
+        } else if vehicles2.is_empty() && !vehicles1.is_empty() {
             let (value1, index1) = vehicles1.pop().unwrap();
             pre_vehi1 = Some((choices.len(), value1));
-            max += value1 as u128 ;
+            max += value1 as u128;
             cap -= 1;
             choices.push(index1);
+        } else {
+            break;
         }
     }
     (max, choices)
+}
+
+#[test]
+pub fn test() {
+    use rand::Rng;
+    use std::time::SystemTime;
+    use std::time::UNIX_EPOCH;
+
+    let mut rng = rand::thread_rng();
+   
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    for _ in 0..10 {
+        let mut vehicles1 = BinaryHeap::new();
+        let mut vehicles2 = BinaryHeap::new();
+        for i in 1..=10000 {
+            let space = rng.gen::<usize>() % 2 + 1;
+            let value = rng.gen::<usize>() % 100;
+            if space == 1 {
+                vehicles1.push((value, i))
+            } else {
+                vehicles2.push((value, i))
+            }
+        }
+        let cap = rng.gen::<usize>() % 10000 + 10000;
+        println!("cap:{}",cap);
+        let (max, _) = search_greedy(&mut vehicles1, &mut vehicles2, cap);
+        println!("max:{}", max);
+        println!()
+    }
+    let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    println!("duration:{}", end.as_millis() - start.as_millis());
 }
